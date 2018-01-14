5059e10e252d874826a4d7d1b084325fea9473a2
rsugasi <sugasi.ramchandra@gmail.com>
2018-01-14 04:25:56 -0500

    adding move command with suppress prompting to overwrite

--- windows/deploy.bat
+++ windows/deploy.bat
@@ -3,4 +3,4 @@ cd %mypath%
 .\mvnw -U -gs settings.xml org.apache.maven.plugins:maven-dependency-plugin:2.8:copy ^
                   -Dartifact=%GROUPID%:%ARTIFACTID%:%VERSION%-SNAPSHOT:%PACKAGING% ^
 		  -Dtransitive=false -DoutputDirectory=. -Dmdep.stripVersion=true
-move *.%PACKAGING% %1
+move /y *.%PACKAGING% %1
