3a7318740a1d9161ec1a8ed1194d6bb58e46de58
rsugasi <sugasi.ramchandra@gmail.com>
2018-01-14 04:46:25 -0500

    adding echo statements

--- windows/deploy.bat
+++ windows/deploy.bat
@@ -4,6 +4,7 @@ cd %mypath%
 .\mvnw -U -gs settings.xml org.apache.maven.plugins:maven-dependency-plugin:2.8:copy ^
                   -Dartifact=%GROUPID%:%ARTIFACTID%:%VERSION%-SNAPSHOT:%PACKAGING% ^
 		  -Dtransitive=false -DoutputDirectory=. -Dmdep.stripVersion=true
+
 SET TARGET_DIR=%1
 echo %1
 echo "------"
