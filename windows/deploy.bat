6a8f9aef881b15c9e96d3ab8360a7d0c3a483663
rsugasi <sugasi.ramchandra@gmail.com>
2018-01-14 04:35:26 -0500

    adding target dir creation if one doesn't exist

--- windows/deploy.bat
+++ windows/deploy.bat
@@ -4,4 +4,8 @@ cd %mypath%
 .\mvnw -U -gs settings.xml org.apache.maven.plugins:maven-dependency-plugin:2.8:copy ^
                   -Dartifact=%GROUPID%:%ARTIFACTID%:%VERSION%-SNAPSHOT:%PACKAGING% ^
 		  -Dtransitive=false -DoutputDirectory=. -Dmdep.stripVersion=true
+SET TARGET_DIR=%1
+if exist %TARGET_DIR% echo "Folder already exists"
+
+if not exist %TARGET_DIR% mkdir %TARGET_DIR%
 move /y *.%PACKAGING% %1
