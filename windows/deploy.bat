61099b4895d28c91de6cc89c2f4b7b3f6009e509
rsugasi <sugasi.ramchandra@gmail.com>
2018-01-14 04:57:37 -0500

    adding echo statements

--- windows/deploy.bat
+++ windows/deploy.bat
@@ -3,7 +3,8 @@ SET mypath=%~dp0
 cd %mypath%
 .\mvnw -U -gs settings.xml org.apache.maven.plugins:maven-dependency-plugin:2.8:copy ^
                   -Dartifact=%GROUPID%:%ARTIFACTID%:%VERSION%-SNAPSHOT:%PACKAGING% ^
-		  -Dtransitive=false -DoutputDirectory=. -Dmdep.stripVersion=true
+		  -Dtransitive=false -DoutputDirectory=. -Dmdep.stripVersion=true ^
+                  && move /y *.%PACKAGING% %1
 
 IF %ERRORLEVEL% NEQ 0 (
   REM do something here to address the error
