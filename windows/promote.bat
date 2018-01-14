13d81801441e9a8b8b5bdf2b0044e0083397decf
sasi <ksasidhar9@gmail.com>
2018-01-14 10:32:13 -0500

    combining to one line

--- windows/promote.bat
+++ windows/promote.bat
@@ -3,9 +3,9 @@ SET mypath=%~dp0
 cd %mypath%
 .\mvnw -U -gs settings.xml  org.apache.maven.plugins:maven-dependency-plugin:2.8:copy ^
                   -Dartifact=%GROUPID%:%ARTIFACTID%:%VERSION%-SNAPSHOT:%PACKAGING% ^
-		  -Dtransitive=false -DoutputDirectory=. -Dmdep.stripVersion=true
-
-.\mvn -gs settings.xml deploy:deploy-file -DgroupId=%GROUPID% ^
+		  -Dtransitive=false -DoutputDirectory=. -Dmdep.stripVersion=true ^
+		   && ^
+		   .\mvn -gs settings.xml deploy:deploy-file -DgroupId=%GROUPID% ^
                         -DartifactId=%ARTIFACTID% ^
 		        -Dversion=%VERSION% ^
 		        -Dpackaging=%PACKAGING% ^
