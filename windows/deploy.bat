8c1fd488640c4a0c05bc96d60cca8e070e68a80a
rsugasi <sugasi.ramchandra@gmail.com>
2018-01-14 04:09:11 -0500

    groupId typo

--- windows/deploy.bat
+++ windows/deploy.bat
@@ -1,6 +1,6 @@
 SET mypath=%~dp0
 cd %mypath%
-.\mvnw -U -gs settings.xml org.apache.maven-plugins:maven-dependency-plugin:2.8.0:copy ^
+.\mvnw -U -gs settings.xml org.apache.maven.plugins:maven-dependency-plugin:2.8:copy ^
                   -Dartifact=%GROUPID%:%ARTIFACTID%:%VERSION%-SNAPSHOT:%PACKAGING% ^
 		  -Dtransitive=false -DoutputDirectory=. -Dmdep.stripVersion=true
 
