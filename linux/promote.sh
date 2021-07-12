3ae128b8860443665bf6a1f6a2402fa4b725991b
Divya Balineni <divyasriece@gmail.com>
2021-07-12 09:36:44 -0400

    updated promote.sh for linux

--- linux/promote.sh
+++ linux/promote.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 ./mvnw -U -gs settings.xml  org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
                   -Dartifact=$GROUPID:$ARTIFACTID:$VERSION-SNAPSHOT:$PACKAGING \
 		  -Dtransitive=false -DoutputDirectory=. -Dmdep.stripVersion=true \
-		   && ^ ./mvnw -gs settings.xml deploy:deploy-file -DgroupId=$GROUPID \
+		   && ./mvnw -gs settings.xml deploy:deploy-file -DgroupId=$GROUPID \
                         -DartifactId=$ARTIFACTID \
 		        -Dversion=$VERSION \
 		        -Dpackaging=$PACKAGING \
