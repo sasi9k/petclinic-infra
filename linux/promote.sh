48839b3bc9c7c1bb339aa09e2480dc8622a54416
vamsyavk <86608054+vamsyavk@users.noreply.github.com>
2021-07-12 08:59:13 -0400

    creating promote.sh

---
+++ linux/promote.sh
@@ -0,0 +1,10 @@
+./mvnw -U -gs settings.xml  org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
+                  -Dartifact=%GROUPID%:%ARTIFACTID%:%VERSION%-SNAPSHOT:%PACKAGING% \
+		  -Dtransitive=false -DoutputDirectory=. -Dmdep.stripVersion=true \
+		   && ^ ./mvnw -gs settings.xml deploy:deploy-file -DgroupId=%GROUPID% \
+                        -DartifactId=%ARTIFACTID% \
+		        -Dversion=%VERSION% \
+		        -Dpackaging=%PACKAGING% \
+		        -Dfile=%ARTIFACTID%.%PACKAGING% \
+		        -DrepositoryId=nexus \
+		        -Durl=%NEXUS_URL%/maven-releases
