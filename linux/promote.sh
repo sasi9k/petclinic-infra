b4533a0984e443357da2f8464cba0cbe49502c1b
Divya Balineni <divyasriece@gmail.com>
2021-07-12 09:35:33 -0400

    updated promote.sh with mvn directory

--- linux/promote.sh
+++ linux/promote.sh
@@ -2,13 +2,13 @@
 export JAVA_HOME=/usr/lib/jvm/default-java
 cd "$(dirname "$0")"
 ./mvnw -U -gs settings.xml  org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
-                  -Dartifact=%GROUPID%:%ARTIFACTID%:%VERSION%-SNAPSHOT:%PACKAGING% \
+                  -Dartifact=$GROUPID:$ARTIFACTID:$VERSION-SNAPSHOT:$PACKAGING \
 		  -Dtransitive=false -DoutputDirectory=. -Dmdep.stripVersion=true \
-		   && ^ ./mvnw -gs settings.xml deploy:deploy-file -DgroupId=%GROUPID% \
-                        -DartifactId=%ARTIFACTID% \
-		        -Dversion=%VERSION% \
-		        -Dpackaging=%PACKAGING% \
-		        -Dfile=%ARTIFACTID%.%PACKAGING% \
+		   && ^ ./mvnw -gs settings.xml deploy:deploy-file -DgroupId=$GROUPID \
+                        -DartifactId=$ARTIFACTID \
+		        -Dversion=$VERSION \
+		        -Dpackaging=$PACKAGING \
+		        -Dfile=$ARTIFACTID.$PACKAGING \
 		        -DrepositoryId=nexus \
-		        -Durl=%NEXUS_URL%/maven-releases
+		        -Durl=$NEXUS_URL/maven-releases
 
